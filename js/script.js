07d9533400dbce0e018fd58d03b5c51b564935c8
LarrikinXIII <13thsnap@gmail.com>
2026-07-21 16:25:05 +0300

    Initialize photo sphere viewer with configuration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+import { Viewer } from '@photo-sphere-viewer/core';
 
+const viewer = new Viewer({
+
+    container: document.querySelector('#viewer'),
+
+    panorama: 'images/livingroom.jpg',
+
+    defaultYaw: 0,
+
+    defaultPitch: 0,
+
+    defaultZoomLvl: 0,
+
+    mousewheel: true,
+
+    touchmoveTwoFingers: false,
+
+    navbar: [
+
+        'zoom',
+
+        'move',
+
+        'fullscreen'
+
+    ],
+
+    loadingImg: null
+
+});
